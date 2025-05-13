39568a4d992fe11d781a4d72ce3205b6496dc74d
VKrishna <103748294+VKrishnaChaitanyaV@users.noreply.github.com>
2025-05-13 11:37:25 +0530

    Update extract-text.js

--- src/pages/api/extract-text.js
+++ src/pages/api/extract-text.js
@@ -4,9 +4,6 @@ export default async function handler(req, res) {
   if(req.method == "GET") {
     return res.status(200).json({ success: 'Get Call.' });
   }
-  else if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
 
   const { base64 } = req.body;
 
