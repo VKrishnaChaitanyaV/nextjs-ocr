476af3b183dac8078b27e21516ae3aeca1a9a049
VKrishna <103748294+VKrishnaChaitanyaV@users.noreply.github.com>
2025-05-13 11:44:09 +0530

    Update extract-text.js

--- src/pages/api/extract-text.js
+++ src/pages/api/extract-text.js
@@ -1,27 +1,23 @@
 import Tesseract from 'tesseract.js';
 
 export default async function handler(req, res) {
-  if(req.method == "GET") {
-    return res.status(200).json({ success: 'Get Call.' });
+  if (req.method == 'GET') {
+    return res.status(200).json({ error: 'Get APi Call!' });
+  }
+  else if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed!' });
   }
 
   const { base64 } = req.body;
 
   if (!base64) {
-    return res.status(400).json({ error: 'No image data provided' });
+    return res.status(400).json({ error: 'No image provided' });
   }
 
   try {
-    const result = await Tesseract.recognize(
-      base64,
-      'eng',
-      {
-        logger: (m) => console.log(m), // Optional: Logs OCR progress
-      }
-    );
-
+    const result = await Tesseract.recognize(base64, 'eng');
     res.status(200).json({ text: result.data.text });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to process image', detail: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
